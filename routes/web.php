2736a4b572289fcc44c89f37480b084bc148945b
Didarul Karim Jewel <m.diarulkarimjewel@gmail.com>
2019-09-09 15:13:31 +0600

    created a initial role and permssion

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+Route::get('/create_role_permission', function () {
+
+    $role = Role::create(['name' => 'Administer']);
+    $permission = Permission::create(['name' => 'Administer roles & permissions']);
+
+    auth()->user()->assignRole('Administer');
+    auth()->user()->givePermissionTo('Administer roles & permissions');
+});
+
+
 
 Route::get('/', function () {
     return view('welcome');
